dfff19437baedbca2a36d4c3bb9e25c07c856d22
Hao Chiang <haomingchiang@gmail.com>
2022-02-09 18:50:29 -0800

    created database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,29 @@
 import express from "express";
 import authRoutes from "./routes/auth";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
-app.use(express.json());
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("Connected to Database");
 
-app.use("/auth", authRoutes);
+    const app = express();
 
-app.get("/", (req, res) => {
-  res.json("signup index");
-});
+    app.use(express.json());
+    app.use("/auth", authRoutes);
 
-app.listen(8080, () => {
-  console.log(`Listening to port 8080`);
-});
+    app.get("/", (req, res) => {
+      res.json("signup index");
+    });
+
+    app.listen(8080, () => {
+      console.log(`Listening to port 8080`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+    throw new Error(error);
+  });
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,13 +1,48 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-  res.json({
-    email,
-    password,
-  });
-});
+router.post(
+  "/signup",
+  body("email").isEmail().withMessage("The email is invalid."),
+  body("password").isLength({ min: 5 }).withMessage("The password is invalid."),
+  async (req, res) => {
+    const validationError = validationResult(req);
+
+    if (!validationError.isEmpty()) {
+      const errors = validationError.array().map((error) => {
+        return {
+          msg: error.msg,
+        };
+      });
+      return res.json({ errors, data: null });
+    }
+
+    const { email, password } = req.body;
+
+    // await User.create({
+    //   email,
+    //   password,
+    // });
+    const user = await User.findOne({ email });
+
+    if (user) {
+      res.json({
+        errors: [
+          {
+            msg: "Email already in use",
+          },
+        ],
+        data: null,
+      });
+    }
+
+    // res.json("created");
+    res.json(user);
+  }
+);
 
 export default router;
